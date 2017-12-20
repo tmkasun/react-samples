1bb763c7c6e63891f51e5272eae331f5a242b68c
Kasun Thennakoon <tmkasun@gmail.com>
2017-12-20 10:43:08 +0530

    Update npm and webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['es2015', 'react']
+                            presets: ['env', 'react']
                         }
                     }
                 ]
